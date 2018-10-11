475e1ef67d5354a08776afde05ce6342e7466f72
GavinSmyth <gavinsmyth1998@gmail.com>
2018-10-11 14:31:01 +0200

    Add files via upload

---
+++ year3GrandPree/G/G/Engine.cs
@@ -0,0 +1,12 @@
+﻿namespace G
+{
+    public class Engine
+    {
+        public bool EngineOn { get; set; }
+
+        public string FuelType { get; set; }
+        public string size { get; set; }
+        public string Type { get; set; }
+
+    
+}
---
+++ year3GrandPree/G/G/Racecar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G
+{
+    public class Racecar
+    {
+        public Engine Engine { get; set; }
+        public String colour { get; set; }
+        public String model { get; set; }
+        public int NumWheels { get; set; }
+        public String make { get; set; }
+        public int DistanceTravelled { get; set; }
+
+        public Racecar()
+        {
+            this.DistanceTravelled = 0;
+
+        }
+
+        public Engine engine { get; set; }
+        
+
+        
+
+        public void move(int meters)
+        {
+            if (this.Engine.EngineOn)
+            {
+                this.DistanceTravelled += meters;
+            }
+        }
+        
+
+        
+
+    }
+}
